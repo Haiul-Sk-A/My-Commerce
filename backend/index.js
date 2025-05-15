d50fe2bf3cf450d638397106d03ea7ac39947d65
Haiul Sk <haiulsk037@gmail.com>
2025-05-15 21:35:12 +0530

    fix:Port Service

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import connectDB from './Database/db.js';
 import dotenv from 'dotenv';
+import route from './routes/userRoute.js';
 dotenv.config()
 
 connectDB();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const app = express()
 
 app.get('/',(req,res)=>{
     res.send("Hello World");
 });
+
+// app.use("/api",route)
 
 app.listen(PORT,()=>{
     console.log(`Server runnning:http://localhost:${PORT}`)
