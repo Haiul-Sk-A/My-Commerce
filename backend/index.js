3bdae2e8b8c062b775709a5096dce3db03e34612
Haiul Sk <haiulsk037@gmail.com>
2025-06-20 07:50:18 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ import connectDB from './Database/db.js';
 // import route from './routes/userRoute.js'
 import authRoutes from './routes/authRoutes.js'
 import dotenv from 'dotenv';
-import productRouter from './routes/productRoutes.js';
 dotenv.config()
 
 connectDB();
@@ -18,7 +17,6 @@ app.get('/',(req,res)=>{
 app.use(express.json());
 
 app.use('/api',authRoutes);
-app.use('/api', productRouter);
 
 app.listen(PORT,'0.0.0.0',()=>{
     console.log(`✅ Server running on localhost:${PORT}`)
--- backend/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-import express, { Router } from 'express'
-import { addProduct } from '../controllers/productController.js';
-
-const productRouter = express.Router();
-
-productRouter.post('/product',addProduct);
-
-export default productRouter;
