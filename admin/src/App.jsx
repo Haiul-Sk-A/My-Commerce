5b60f1185128f3da53d0b1dc12af60bd6eaf7ea6
Haiul Sk <haiulsk037@gmail.com>
2025-06-22 17:39:41 +0530

    admin

---
+++ admin/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-blue-600 text-white text-3xl font-bold">
+        Tailwind is working!
+      </div>
+    </>
+  )
+}
+
+export default App
